f4d24150c036f73b1ff678ea333aad1271830233
kid424 <darkred424@gmail.com>
2024-07-18 11:07:04 +0800

    add luci-app-xupnpd for immortal

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -32,6 +32,7 @@ UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 #UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 #UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+UPDATE_PACKAGE "luci-app-xupnpd" "jarod360/luci-app-xupnpd" "main"
 
 #if [[ $WRT_REPO != *"lede"* ]]; then
 #	UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
